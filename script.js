51bd5371b32d504b3b024becf18096c3ec2e99eb
Liebe974 <137163247+Liebe974@users.noreply.github.com>
2023-10-31 15:32:41 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+const container = document.querySelector(".container");
+
+// Créer un portfolio pour stocker les utilisateurs dans un tableau
+let users = [];
+fetch("https://randomuser.me/api/?results=20")
+.then((response) => response.json())
+.then((data) => {
+
+    // Ajouter les utilisateurs dans le tableau
+    users = data.results;
+    // Afficher les utilisateurs dans le container
+    displayUsers();
+});
+
+// Afficher les utilisateurs dans le container
+function displayUsers() {
+    container.innerHTML = "";
+    users.forEach((user) => {
+        const div = document.createElement("div");
+        div.classList.add("user");
+        div.innerHTML = `
+        <img src="${user.picture.large}" alt="profile picture">
+        <h3> ${user.name.title} ${user.name.first} ${user.name.last}</h3>   
+        <p>${user.gender.toUpperCase()}</p>
+        <p>${user.email}</p>
+        `;
+        container.appendChild(div);
+    })};
